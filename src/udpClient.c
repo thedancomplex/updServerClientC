1f16bb66fed046ce776e61f72c23cda8f18a0117
Daniel M. Lofaro <dan@danlofaro.com>
2012-11-10 18:43:16 -0500

    added ach to server and client

--- src/udpClient.c
+++ src/udpClient.c
@@ -4,6 +4,20 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include "../../hubo-ach/include/hubo.h"
+// for ach
+#include <errno.h>
+#include <fcntl.h>
+#include <assert.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <ctype.h>
+#include <stdbool.h>
+#include <math.h>
+#include <inttypes.h>
+#include "ach.h"
+
+
 #define BUFLEN 512
 #define NPACK 1000
 #define PORT 9930
@@ -11,6 +25,13 @@
 
 #define SRV_IP "192.168.0.10"
 /* diep(), #includes and #defines like in the server */
+
+// ach channels
+ach_channel_t chan_hubo_ref;      // hubo-ach
+
+
+int hubo_debug = 0;
+
 int main(void)
 {
   struct sockaddr_in si_other;
@@ -26,10 +47,35 @@ int main(void)
       exit(1);
     }
 
+
+
+    size_t fs;
+    /* open ach channel */
+    int r = ach_open(&chan_hubo_ref, HUBO_CHAN_REF_NAME , NULL);
+    assert( ACH_OK == r );
+
+
+    struct hubo_ref H_ref;
+    memset( &H_ref, 0, sizeof(H_ref));
+    char newBuff[sizeof(H_ref)];
+    memcpy(&newBuff, &H_ref, sizeof(H_ref));
+
+
+
+
+    r = ach_get( &chan_hubo_ref, &H_ref, sizeof(H_ref), &fs, NULL, ACH_O_LAST );
+    if(ACH_OK != r) {
+        if(hubo_debug) {
+            printf("Ref ini r = %s\n",ach_result_to_string(r));}
+         }
+    else{   assert( sizeof(H_ref) == fs ); }
+
+
     for (i=0; i<NPACK; i++) {
       printf("Sending packet %d\n", i);
       sprintf(buf, "This is packet %d\n", i);
-      if (sendto(s, buf, BUFLEN, 0, &si_other, slen)==-1)
+      //if (sendto(s, buf, BUFLEN, 0, &si_other, slen)==-1)
+      if (sendto(s, newBuff, sizeof(newBuff), 0, &si_other, slen)==-1)
         printf("sendto()");
     }
 
--- src/udpServer.c
+++ src/udpServer.c
@@ -4,9 +4,32 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include "../../hubo-ach/include/hubo.h"
+// for ach
+#include <errno.h>
+#include <fcntl.h>
+#include <assert.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <ctype.h>
+#include <stdbool.h>
+#include <math.h>
+#include <inttypes.h>
+#include "ach.h"
+
+
 #define BUFLEN 512
-#define NPACK 10
+#define NPACK 1000
 #define PORT 9930
+
+
+#define SRV_IP "192.168.0.10"
+/* diep(), #includes and #defines like in the server */
+
+// ach channels
+ach_channel_t chan_hubo_ref;      // hubo-ach
+
+
 void diep(char *s)
 {
   perror(s);
@@ -25,12 +48,31 @@ int main(void)
   si_me.sin_addr.s_addr = htonl(INADDR_ANY);
   if (bind(s, &si_me, sizeof(si_me))==-1)
       diep("bind");
+
+
+  size_t fs;
+  /* open ach channel */
+  int r = ach_open(&chan_hubo_ref, HUBO_CHAN_REF_NAME , NULL);
+  assert( ACH_OK == r );
+
+
+  struct hubo_ref H_ref;
+  memset( &H_ref, 0, sizeof(H_ref));
+  char newBuff[sizeof(H_ref)];
+  memcpy(&newBuff, &H_ref, sizeof(H_ref));
+
+  ach_put( &chan_hubo_ref, &H_ref, sizeof(H_ref));
+
+
+
+/*
   for (i=0; i<NPACK; i++) {
     if (recvfrom(s, buf, BUFLEN, 0, &si_other, &slen)==-1)
       diep("recvfrom()");
     printf("Received packet from %s:%d\nData: %s\n\n", 
             inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port), buf);
   }
+*/
   close(s);
   return 0;
 }
